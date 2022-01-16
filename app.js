3b3ea76a31e57c538f2e5b37aa93887b4b563f39
Laura <lmcsay@gmail.com>
2022-01-15 23:09:50 -0600

    Add form submission response

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const navBtn = document.getElementById('nav-btn');
 const navbar = document.getElementById('navbar');
 const navClose = document.getElementById('nav-close');
+const result = document.querySelector('.result');
+const submitBtn = document.querySelector('.submit-btn');
 // show nav
 navBtn.addEventListener('click', () => {
   navbar.classList.add('showNav');
@@ -10,6 +12,20 @@ navBtn.addEventListener('click', () => {
 navClose.addEventListener('click', () => {
   navbar.classList.remove('showNav');
 });
+// show result
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  result.style.opacity = 1;
+  hideResult();
+});
+
+const hideResult = () => {
+  setTimeout(() => {
+    result.style.opacity = 0;
+  }, 3000);
+};
+
 //setup date
 const date = (document.getElementById('date').innerHTML =
   new Date().getFullYear());
+
